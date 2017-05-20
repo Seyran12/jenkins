53e4bc6e3fb4d07d3b40cdca3817982e9e28564e
Seyran Abdullayev <s.abdullayevjb@gmail.com>
2017-05-20 01:15:22 -0700

    second commit

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -7,6 +7,6 @@ public class App {
         System.out.println("Schoolmaster and The classroom is an anagram?-"+ Anagram.validate("Schoolmaster","The classroom"));
         System.out.println("The Hurricanes and These churn air is an anagram?-" +Anagram.validate("The Hurricanes", "These churn air"));
         System.out.println("Astronomers and No more stars is an anagram?-"+ Anagram.validate("Astronomers", "No more stars"));
-        System.out.println("Oxrenet and Ne Xrena Sebe is an anagram?-" + Anagram.validate("Oxrenet", "Ne Xrena Sebe"));
+       //System.out.println("Oxrenet and Ne Xrena Sebe is an anagram?-" + Anagram.validate("Oxrenet", "Ne Xrena Sebe"));
     }
 }
